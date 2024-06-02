71d1b55768dd3cd367599eedb90c708f8c5796c9
uvaisantilia <uvais@antiliasolutions.com>
2024-06-02 17:13:30 +0300

    02062024

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,133 @@
-const Login = () => {
-    return (
-        <div>Login</div>
-    )
-}
+"use client";
 
-export default Login
+import { useWixClient } from "@/hooks/useWixClient";
+import { useState } from "react";
+
+enum MODE {
+  LOGIN = "LOGIN",
+  REGISTER = "REGISTER",
+  RESET_PASSWORD = "RESET_PASSWORD",
+  EMAIL_VERIFICATION = "EMAIL_VERIFICATION",
+}
+const Login = () => {
+  const [mode, setMode] = useState(MODE.LOGIN);
+
+  const [username, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailCode, setEmailCode] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [message, setMessage] = useState("");
+
+  const formTitle =
+    mode === MODE.LOGIN
+      ? "Log in"
+      : mode === MODE.REGISTER
+      ? "Register"
+      : mode === MODE.RESET_PASSWORD
+      ? "Reset Password"
+      : "Verify Email";
+
+  const buttonTitle =
+    mode === MODE.LOGIN
+      ? "Login"
+      : mode === MODE.REGISTER
+      ? "Register"
+      : mode === MODE.RESET_PASSWORD
+      ? "Reset"
+      : "Verify";
+
+  const wixClient = useWixClient();
+
+  return (
+    <div className="h-[calc(100vh-80px)]px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 flex items-start justify-center">
+      <form className="flex flex-col gap-8 p-8">
+        <h1 className="text-3xl font-semibold">{formTitle}</h1>
+        {mode === MODE.REGISTER ? (
+          <div className="flex flex-col gap-2">
+            <label className="text-sm text-gray-700">Username</label>
+            <input
+              type="text"
+              name="username"
+              placeholder="john"
+              className="ring-2 ring-gray-300 rounded-md p-4 "
+            />
+          </div>
+        ) : null}
+        {mode !== MODE.EMAIL_VERIFICATION ? (
+          <div className="flex flex-col gap-2">
+            <label className="text-sm text-gray-700">Email</label>
+            <input
+              type="email"
+              name="email"
+              placeholder="john@gmail.com"
+              className="ring-2 ring-gray-300 rounded-md p-4 "
+            />
+          </div>
+        ) : (
+          <div className="flex flex-col gap-2">
+            <label className="text-sm text-gray-700">Verification Code</label>
+            <input
+              type="text"
+              name="emailCode"
+              placeholder="Code"
+              className="ring-2 ring-gray-300 rounded-md p-4 "
+            />
+          </div>
+        )}
+        {mode === MODE.LOGIN || mode === MODE.REGISTER ? (
+          <div className="flex flex-col gap-2">
+            <label className="text-sm text-gray-700">Password</label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Enter your password"
+              className="ring-2 ring-gray-300 rounded-md p-4 "
+            />
+          </div>
+        ) : null}
+        <button
+          className="bg-lama text-white p-2 rounded-md disabled:bg-pink-200 disabled:cursor-not-allowed"
+          disabled={isLoading}
+        >
+          {isLoading ? "Loading..." : buttonTitle}
+        </button>
+        {mode === MODE.LOGIN && (
+          <div
+            className="text-sm underline cursor-pointer"
+            onClick={() => setMode(MODE.RESET_PASSWORD)}
+          ></div>
+        )}
+        {error && <div className="text-red-600">{error}</div>}
+        {mode == MODE.LOGIN && (
+          <div
+            className="text-sm underline cursor-pointer"
+            onClick={() => setMode(MODE.REGISTER)}
+          >
+            {"Don't"} have an account?
+          </div>
+        )}
+        {mode == MODE.REGISTER && (
+          <div
+            className="text-sm underline cursor-pointer"
+            onClick={() => setMode(MODE.LOGIN)}
+          >
+            Have an account?
+          </div>
+        )}
+        {mode == MODE.RESET_PASSWORD && (
+          <div
+            className="text-sm underline cursor-pointer"
+            onClick={() => setMode(MODE.LOGIN)}
+          >
+            Go back to Login
+          </div>
+        )}
+        {message && <div className="text-green-600 text-sm">{message}</div>}
+      </form>
+    </div>
+  );
+};
+
+export default Login;
